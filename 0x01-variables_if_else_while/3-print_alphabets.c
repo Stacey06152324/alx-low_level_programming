659335da7f18a434a38ac30ef4357284bfb17383
Stacey06152324 <svanwyk722@gmail.com>
2022-09-10 00:03:12 -0700

    Task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,18 +1,27 @@
-#include <stdio.h>
- 
+#include <stdlib.h>
+#include <time.h>
+/** 
+ * main - prints the letters of the alphabet 
+ *
+ * Description: prints the letter of the alphabet
+ */
+
+
 int main(void)
+{
+	int n;
 
-{
-char ch;
-for (ch = 'a' ; ch < = 'z' ; ch++)
-{
-        putchar(ch);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	char ch;
+	for (ch = 'a' ;ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A' ;ch <= 'Z' ; ch++)
+	{
+		putchar(ch)
+	}
+	putchar('\n');
+	return (0);
 }
-for (ch = 'A' ; ch < = 'Z' ; ch++)
-{
-	putchar(ch);
-}
-putchar('\n');
-return(0);
-}
-
