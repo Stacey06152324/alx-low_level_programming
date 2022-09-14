a6d680984a5f6a5e9f2ad5c25575aaf3db6d69b5
Stacey06152324 <svanwyk722@gmail.com>
2022-09-13 21:10:38 -0700

    My second commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,8 +5,13 @@
  *
  * Return: Always 0.
  */
-int main(void)
+int print_alphabet(void)
 {
-    print_alphabet();
-    return (0);
+    char c ;
+
+    for (c = 'a' ; c <= 'z' ; c++)
+    {
+	    _putchar(c);
+    }
+    _putchar('\n');
 }
