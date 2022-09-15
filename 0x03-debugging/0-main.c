143a04543be6003956c1d5949b1e20dcb4907612
Stacey06152324 <svanwyk722@gmail.com>
2022-09-15 10:41:47 -0700

    My first debug commit

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+* main - print last digit of int
+* Return: 0 (Successful)
+*/
+int main(void)
+{
+	int n;
+	int last;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	last = n % 10;
+	if (last == 0)
+		printf("%i is zero\n", n);
+	if (last > 0)
+		printf("%i is positive\n", n);
+	if (last < 0)
+		printf("%i is negative\n", n);
+
+	return (0);
+}
