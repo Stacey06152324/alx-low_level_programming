329427f6c933290e8d1a2702360c5203e69598c9
Stacey06152324 <svanwyk722@gmail.com>
2022-09-15 16:50:58 -0700

    MyCommit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,29 +1,18 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
-/* more headers goes there */
+#include "main.h"
 
-/* betty style doc for function main goes there */
 /**
-* main - print last digit of int
-* Return: 0 (Successful)
+* main - tests function that prints if integer is positive or negative
+* Return: 0
 */
+
 int main(void)
 {
-	int n;
-	int last;
+int i;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	last = n % 10;
-	if (last == 0)
-		printf("%i is zero\n", n);
-	if (last > 0)
-		printf("%i is positive\n", n);
-	if (last < 0)
-		printf("%i is negative\n", n);
-
-	return (0);
+i = 0;
+        
+positive_or_negative(i);
+      
+return (0);
 }
 
