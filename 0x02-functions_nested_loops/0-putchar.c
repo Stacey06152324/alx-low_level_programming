1413679f800bece062fed0c138cd806b7cd0d22b
Stacey06152324 <svanwyk722@gmail.com>
2022-09-15 01:51:48 -0700

    1. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,11 @@
-#include "main.h"
-
+#include<stdio.h>
 /**
- * main- print putchar
- * Return : Always 0
- */
-
+* main - Print
+* Return: 0. Successful.
+*/
 int main(void)
-
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-
+	printf("_putchar\n");
 	return (0);
 }
-
 
