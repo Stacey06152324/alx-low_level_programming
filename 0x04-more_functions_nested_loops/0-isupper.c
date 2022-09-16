b23529854f080e17dd9d24b2252188c9c23a8e62
Stacey06152324 <svanwyk722@gmail.com>
2022-09-16 16:42:56 -0700

    My Uppercase commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ */
+int _isupper(int c)
+{
+	if (c>='A' && c<'Z')
+		return (1);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN.H
+#define MAIN>H		
+
+/* this prototype has to be declared in a header file */
+
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_sqare(int size);
+void print_triagle(int size);
+
+#endif /*_MAIN_H_*/
