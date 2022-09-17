ef282badaf52cbd97935909ef2dd3247b820c755
Stacey06152324 <svanwyk722@gmail.com>
2022-09-17 06:54:52 -0700

    My upper commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,18 @@
 #include <unistd.h>
+#include <ctype.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- */
-int _isupper(int c)
-{
-		if (c >='A' && c <= 'Z')
+* _putchar - writes the character c to stdout
+* @c: The character to print
+*
+* Return: On success 1.
+*/
 
-			return (1);
-        	else 
-			return (0);
+int isupper(int c)
+{
+	if (c >='A' && c <= 'Z')
+
+		return (1);
+        else 
+		return (0);
 }
