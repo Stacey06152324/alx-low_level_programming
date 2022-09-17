971f39a35e9ad06914b51c466c06a1ebaf4515d5
Stacey Van Wyk <111110823+Stacey06152324@users.noreply.github.com>
2022-09-17 15:38:08 +0200

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,15 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- */
+* _putchar - writes the character c to stdout
+* @c: The character to print
+*
+* Return: On success 1.
+*/
 int _isupper(int c)
 {
-		if (c >='A' && c <= 'Z')
-
-			return (1);
-        	else 
-			return (0);
+	if (c >='A' && c <= 'Z')
+		return (1);
+	else 
+		return (0);
 }
