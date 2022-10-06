2b26ec0781a3ac475bbadbe71f8c5daae47be0d4
Stacey06152324 <svanwyk722@gmail.com>
2022-10-06 01:26:09 -0700

    My second commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,16 @@
 #include "main.h"
+
 /**
- * reset_to_98_ -function that takes a pointer to an int as parameter and
- * updates the value it points to 98
+ * reset_to_98_ - function that takes a pointer to an int as parameter and
+ 
+ * updates the value its point to 98
  *
  * @n: This is the entry
  *
  **/
 
 void reset_to_98(int *n)
+
 {
 	*n =98;
 }
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - functions that swap the value of two intergers
+ * @a: this is the first entry point
+ * @b: this is the second entry point
+ */
+
+void swap_int(int *a, int *b)
+}
+	int changed;
+
+	changed = *a;
+	*a = *b;
+	b* = changed;
+	
+			
+} 
