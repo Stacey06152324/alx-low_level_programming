9339b7ecdd64392d4fa575ecdfbcb7e140cad607
Stacey Van Wyk <111110823+Stacey06152324@users.noreply.github.com>
2022-10-18 00:42:16 +0200

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -6,10 +6,12 @@
 
 */
 void _puts(char *str)
-{       
-    //string initialisation
-    char Mystr[]= "stdout";
+
+{
+	//string initialisation
+    char Mystr[] = "C and C++";
+    
     puts(Mystr); //writing the string to stdout
-  
+    
     return 0;
-}
+}    
