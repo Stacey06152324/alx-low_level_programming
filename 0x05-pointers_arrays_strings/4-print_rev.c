128894b14aacba3de93ef9fea92a9d1ff16e5150
Stacey Van Wyk <111110823+Stacey06152324@users.noreply.github.com>
2022-10-18 01:17:05 +0200

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,13 +1,22 @@
 #include "main.h"
 
-**/
+/**
   *print_rev - functions that prints a string in reverse
   *@str: This is my first entry
   */
   
 void print_rev(char *s)
 {
-  char str[40]; // declare the size of character string.
-  printf (" \n Enter a string to be reversed: ");
-  scanf ("%s", str);
-// use strrev() function to reverse a string.
+	int i = 0;
+	int j;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	for (j = i - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+}
