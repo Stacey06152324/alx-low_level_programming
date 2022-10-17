b53e26c546ad541b153b9c09a6bf4f0a70d8d31a
Stacey Van Wyk <111110823+Stacey06152324@users.noreply.github.com>
2022-10-18 00:39:19 +0200

    Create 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+*_puts -function that prints a string
+*@s: this is the first entry point
+
+*/
+void _puts(char *str)
+{       
+    //string initialisation
+    char Mystr[]= "stdout";
+    puts(Mystr); //writing the string to stdout
+  
+    return 0;
+}
