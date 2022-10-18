f7812c9c771c9166c7cb1346931c7cb3c0975870
Stacey Van Wyk <111110823+Stacey06152324@users.noreply.github.com>
2022-10-18 02:30:26 +0200

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 /**
 *print_array - Function that prints elements of arrays
-@a:This is my firts entry
-@n:This is my seceond entry
+*@a:This is my firts entry
+@*n:This is my seceond entry
 */
 void print_array(int *a, int n)
 {
