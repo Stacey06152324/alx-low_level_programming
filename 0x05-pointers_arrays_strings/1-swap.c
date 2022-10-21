9f331090fe350f673a7d524ac6f212109e2fa1a3
Stacey Van Wyk <111110823+Stacey06152324@users.noreply.github.com>
2022-10-21 13:09:06 +0200

    Update 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * swap_int - functions that swap the value of two intergers
- * @a: this is the first entry point
- * @b: this is the second entry point
+ * swap_int - swaps integers
+ *
+ * @a: this is my first entry point
+ * @b: second entry point
  */
-
 void swap_int(int *a, int *b)
 {
 	int c = *b;
 	int d = *a;
 	*a = c;
 	*b = d;
-} 
+}
